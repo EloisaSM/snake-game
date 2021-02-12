659963810c319b7d2d4822e409125728059a790a
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-02-12 10:21:49 -0300

    create food

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right"
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -20,9 +24,29 @@ function creatSnake() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != "right")  direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame() {
+    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].x = 0;
+    if(snake[0].y < 0  && direction == 'up') snake[0].x = 16 * box;
+
     createBG()
     creatSnake()
+    drawFood()
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -32,10 +56,10 @@ function startGame() {
             snakeX += box;
             break;
         case "left" :
-            snakeX += box;
+            snakeX -= box;
             break;
         case "up": 
-            snakeY += box;
+            snakeY -= box;
             break;
         case "down":
             snakeY += box;
